3f3a8f5db048b2f54dbd1568aa45a90ed54fec0a
manuo <ojhapranav1999@gmail.com>
2024-11-22 11:14:06 +0530

    adding files after the nav creation and router addition

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { Main_top } from './Components/Main_top';
+import {createBrowserRouter,RouterProvider,Route,Link} from "react-router-dom";
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <h1>Hi this is my port</h1>
-  </React.StrictMode>
+  <Main_top/>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-
