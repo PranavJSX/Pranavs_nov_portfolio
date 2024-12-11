414429ea92fc86495ef17ec532328bec26ed2c03
manuo <ojhapranav1999@gmail.com>
2024-12-11 17:18:23 +0530

    updating the footer and the scroll animation

--- src/Components/Home/Footer.jsx
+++ src/Components/Home/Footer.jsx
@@ -11,12 +11,26 @@ export const Footer = () => {
     <div>
         <div className='mt-[50vh] mb-[6.5vh] h-[30vh] footer_main'>
             <div className='ml-[50vh] pt-5 flex-cols'>
-                <div className='pb-2 ml-[-0.4vw] text-2xl text-rose-500 satisfy-regular'>Let's Connect :</div>
-                <div className='social_icons scale-[2] content-center '><a href='https://www.linkedin.com/in/pranav-ojha-14007a1a9/' target='_blank'><FaLinkedin /></a></div>
-                <div className='social_icons scale-[2] content-center mt-5'><a href='https://github.com/PranavJSX' target='_blank'><FaSquareGithub /></a></div>
-                <div className='social_icons scale-[2] content-center mt-5'><a href='mailto:ojhapranav1999@gmail.com' target='_blank'><IoMail /></a></div>
+                <div className='pb-2 ml-[-0.4vw] text-2xl text-rose-700 satisfy-regular'>Let's Connect :</div>
+                <div className='social_icons scale-[1.6] content-center flex-row ml-[17vw] flex'>
+                    <div className='pt-[0.7vh]'><a href='www.linkedin.com/in/pranav-ojha-14007a1a9' target='_blank'><FaLinkedin/></a></div>
+                    <div className='scale-[0.7] '>
+                        <a href='www.linkedin.com/in/pranav-ojha-14007a1a9' className='underline_links'  target='_blank'>LinkedIN</a>
+                        </div>
+                </div>
+                <div className='social_icons scale-[1.6] content-center mt-5 ml-[17vw]'>
+                    <div className=''><a href='https://github.com/PranavJSX' target='_blank'><FaSquareGithub /></a></div>
+                    <div className='scale-[0.7] ml-[-8vw] mt-[-2.8vh]'>
+                        <a className='underline_links' href='https://github.com/PranavJSX' target='_blank'>Github</a></div>
+                </div>
+                <div className='social_icons scale-[1.6] content-center mt-5 ml-[17vw]'>
+                    <div><a href='mailto:ojhapranav1999@gmail.com' target='_blank'><IoMail /></a></div>
+                    <div className='scale-[0.7] ml-[-8vw] mt-[-2.8vh]'>
+                        <a className='underline_links' href='mailto:ojhapranav1999@gmail.com' target='_blank'>E-mail</a></div>
+                </div>
             </div>
-        <div className='ml-[140vh] flex'><div className='text-slate-950'>MADE WITH</div><div className='pl-1 pt-1 text-rose-400'> <FaHeart /></div></div>
+        <div className='ml-[140vh] flex mt-[-5vw]'><div className='text-slate-950 '>
+            MADE WITH</div><div className='pl-1 pt-1 text-rose-400'> <FaHeart /></div></div>
         </div>
     </div>
   )
