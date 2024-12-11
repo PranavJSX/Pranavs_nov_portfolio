cfc9c198063d6c2ed72ec63a8274450db04f23a5
manuo <ojhapranav1999@gmail.com>
2024-12-11 16:29:52 +0530

    updating the footer and the scroll animation

--- src/Components/Home/Project_cards.jsx
+++ src/Components/Home/Project_cards.jsx
@@ -18,11 +18,14 @@ export const Project_cards = ({arr}) => {
   return (
     <motion.div ref={ref} initial={{opacity:0}} style={{opacity:opacity,y}} className=' text-rose-900 h-[40vh] p-2'>
         <div className='text-xl text-slate-200'>
-        <h2>
-        {arr.project_heading}
-        </h2>
+            <div className='bg-rose-500 text-gray-800 rounded-lg'>    
+                <h2>
+                {arr.project_heading}
+                </h2>
+            </div>
         </div>
-        <div className='overflow-hidden max-h-[100%] text-center pt-5 text-[2.0vh]'>{arr.info}</div>
+        <div className='overflow-hidden rounded-lg max-h-[100%] text-center mt-2 h-[70%] text-[1.6vh] bg-slate-200 p-5'>
+            {arr.info}</div>
 
     </motion.div>
   )
